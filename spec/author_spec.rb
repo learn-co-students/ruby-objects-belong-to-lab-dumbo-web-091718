7f26e957770af3d041c8b4912d185a2d4d876c02
steven <szapata427@yahoo.com>
2018-09-25 09:02:41 -0400

    Done.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,9 +1,13 @@
 require "spec_helper"
 
-describe "Author" do 
+describe "Author" do
   it "has a name" do
     author = Author.new
-    author.name = "Uncle Bob" 
+    author.name = "Uncle Bob"
     expect(author.name).to eq("Uncle Bob")
+  end
+  it "has a default age" do
+    author = Author.new
+    expect(author.age).to eq(25)
   end
 end
