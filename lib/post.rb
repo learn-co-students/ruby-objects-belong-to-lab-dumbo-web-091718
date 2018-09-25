402c95ddfa94e7051ff6bac8fa71f2299654e47f
github username <github email address>
2018-09-25 19:27:50 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post
+
+  attr_accessor :title, :author
+
+
+def titles(title)
+@title = title
+end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :title, :artist
+
+def songs(title)
+@title = title
+end
+
+
+end
